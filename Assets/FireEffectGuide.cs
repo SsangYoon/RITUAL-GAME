1abecfaed9164c68c2d5af3e32bcc598e7a34e98
Ssangyun <sangyun3631@gmail.com>
2016-01-31 16:11:40 +0900

    Complte

--- Assets/FireEffectGuide.cs
+++ Assets/FireEffectGuide.cs
@@ -1,41 +1,35 @@
 ﻿using UnityEngine;
-using UnityEngine.UI;
-
 using System.Collections;
 
 using SacrificeContoll;
 
 namespace Summon
 {
-	public class SummonGuideController : MonoBehaviour
-	{
+	public class FireEffectGuide : MonoBehaviour {
+
 		private SacrificeManager _SacrifceManager;
 
 		private bool _Enable = false;
 
 		// Use this for initialization
-		private void Awake()
+		void Awake()
 		{
 			_SacrifceManager = GameObject.FindGameObjectWithTag("DataManager").GetComponent<SacrificeManager>();
 		}
 
 		// Update is called once per frame
-		private void Update()
+		void Update()
 		{
 			_Enable = false;
 
-			foreach (int id in _SacrifceManager._SacrifceList)
-			{
-				if (id == (int)ResourceIndex.HUMAN)
-				{
-					_Enable = true;
-				}
-			}
+			if (_SacrifceManager._SacrifceList.Count > 0)
+				_Enable = true;
 
 			if (_Enable)
-				this.GetComponent<Image>().enabled = true;
+				this.GetComponent<SpriteRenderer>().enabled = true;
 			else
-				this.GetComponent<Image>().enabled = false;
+				this.GetComponent<SpriteRenderer>().enabled = false;
 		}
 	}
 }
+
